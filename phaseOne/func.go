ce80a8a619276a3371e1c0ddf3ec7e55254b09b7
tikasan <satak47cpc@gmail.com>
2017-02-04 22:26:01 +0900

    add: 作業

--- phaseOne/func.go
+++ phaseOne/func.go
@@ -13,7 +13,9 @@ import (
 
 // 下みたいなstringで返せばパイソンさんが頑張ってくれます！
 // お願いします！がんばってください！
-var res = `
+
+/*
+フォーマット例
 [
     {
         "category": "1",
@@ -28,12 +30,20 @@ var res = `
         "https://s3-us-west-2.amazonaws.com/lineapitest/bread_240.jpeg"
     }
 ]
-`
+*/
+
+// 実際のデータ
+var res = `[ { "category": "1", "imageUrl": "https://s3-us-west-2.amazonaws.com/lineapitest/hamburger_240.jpeg" }, { "category": "2", "imageUrl": "https://s3-us-west-2.amazonaws.com/lineapitest/pizza_240.jpeg" }, { "category": "3", "imageUrl": "https://s3-us-west-2.amazonaws.com/lineapitest/bread_240.jpeg" } ]`
 
 type PhaseOne struct {
 	DB *sql.DB
 }
 
 func (p *PhaseOne) Response(c echo.Context) error {
-	return c.JSON(http.StatusOK, res)
+	// 乱数で送信するカテゴリ画像とカテゴリNoを決定する
+
+	// Jsonフォーマットに沿ったstringを作成する
+	// ※構造体でいい感じに出来るなら、それでもおｋ。出来上がればなんでも！
+
+	return c.String(http.StatusOK, res)
 }
