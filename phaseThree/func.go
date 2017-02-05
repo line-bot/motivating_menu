fbbb773512e4817289da6cf727855e0baf999a58
tikasan <satak47cpc@gmail.com>
2017-02-05 11:36:24 +0900

    deployのバージョンを上げた

---
+++ phaseThree/func.go
@@ -0,0 +1,28 @@
+package phaseTwo
+
+import (
+	"database/sql"
+	"net/http"
+
+	"fmt"
+	"os"
+
+	"github.com/labstack/echo"
+	"github.com/line-bot/motivating_menu/model"
+	"strconv"
+)
+
+type PhaseTwo struct {
+	DB *sql.DB
+}
+
+func (p *PhaseTwo) Response(c echo.Context) error {
+	categoryId := c.QueryParam("category_id")
+	ci, _ := strconv.Atoi(categoryId)
+	recipe, err := model.RecipeRecommend(p.DB, ci)
+	if err != nil {
+		fmt.Fprint(os.Stderr, err)
+	}
+	c.Response().Header().Set("Content-Type", "application/json")
+	return c.JSON(http.StatusOK, recipe)
+}
